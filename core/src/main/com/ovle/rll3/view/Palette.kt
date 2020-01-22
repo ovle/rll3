cf2cd6ff9c27e99eacb5b1c69e9551590fc8c9ff
ovle <owlywhy@gmail.com>
2020-01-23 01:03:38 +0700

    in-game palettes

--- core/src/main/com/ovle/rll3/view/Palette.kt
+++ core/src/main/com/ovle/rll3/view/Palette.kt
@@ -3,30 +3,67 @@ package com.ovle.rll3.view
 import com.badlogic.gdx.graphics.Color
 
 object Palette {
-    val blackColor: Color = Color.valueOf("272744ff")
-    val darkColor: Color = Color.valueOf("494d7eff")
-    val middleColor: Color = Color.valueOf("8b6d9cff")
-    val lightRedColor: Color = Color.valueOf("c69fa5ff")
-//    val lightYellowColor: Color = Color.valueOf("f2d3abff")
-    val whiteColor: Color = Color.valueOf("fbf5efff")
-
-    private val orderedColors = arrayOf(
-        blackColor, darkColor, middleColor, lightRedColor, /*lightYellowColor,*/ whiteColor
+    private val mappedPalette = arrayOf(
+        Color.valueOf("140C1Cff"),
+        Color.valueOf("30346Dff"),
+        Color.valueOf("597DCEff"),
+        Color.valueOf("DEEED6ff")
     )
 
+    private val palette1 = arrayOf(
+        Color.valueOf("140C1Cff"),
+        Color.valueOf("4E4A4Eff"),
+        Color.valueOf("D04648ff"),
+        Color.valueOf("DAD45Eff")
+    )
+
+    private val palette2 = arrayOf(
+        Color.valueOf("140C1Cff"),
+        Color.valueOf("442434ff"),
+        Color.valueOf("346524ff"),
+        Color.valueOf("6DAA2Cff")
+    )
+
+    private val palette3 = arrayOf(
+        Color.valueOf("140C1Cff"),
+        Color.valueOf("854C30ff"),
+        Color.valueOf("D27D2Cff"),
+        Color.valueOf("DEEED6ff")
+    )
+
+    private val palette4 = arrayOf(
+        Color.valueOf("140C1Cff"),
+        Color.valueOf("4E4A4Eff"),
+        Color.valueOf("757161ff"),
+        Color.valueOf("DEEED6ff")
+    )
+
+    private val palette = palette4
+
+    val blackColor: Color = palette.first()
+    val whiteColor: Color = palette.last()
+
     fun next(color: Color): Color {
-        return when (val index = orderedColors.indexOf(color)) {
+        //todo test
+        if (color == blackColor) return color
+
+        return when (val index = palette.indexOf(color)) {
             -1 -> color                         //out of palette
-            orderedColors.size - 1 -> color     //already last
-            else -> orderedColors[index + 1]
+            palette.size - 1 -> color     //already last
+            else -> palette[index + 1]
         }
     }
 
     fun prev(color: Color): Color {
-        return when (val index = orderedColors.indexOf(color)) {
+        return when (val index = palette.indexOf(color)) {
             -1 -> color                         //out of palette
             0 -> color                          //already first
-            else -> orderedColors[index - 1]
+            else -> palette[index - 1]
         }
+    }
+
+    fun map(color: Color): Color {
+        val index = mappedPalette.indexOf(color)
+        return if (index == -1) color else palette[index]
     }
 }
