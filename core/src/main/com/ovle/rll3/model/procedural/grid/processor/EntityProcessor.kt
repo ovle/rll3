a508752acba488a5db02690c33935c5225171d53
ovle <owlywhy@gmail.com>
2020-05-24 21:55:33 +0700

    spawn table

--- core/src/main/com/ovle/rll3/model/procedural/grid/processor/EntityProcessor.kt
+++ core/src/main/com/ovle/rll3/model/procedural/grid/processor/EntityProcessor.kt
@@ -5,16 +5,12 @@ import com.badlogic.ashley.core.Entity
 import com.ovle.rll3.model.ecs.component.special.LevelInfo
 import com.ovle.rll3.model.ecs.component.special.WorldInfo
 import com.ovle.rll3.model.ecs.component.util.Mappers.position
-import com.ovle.rll3.model.ecs.component.util.Mappers.template
 import com.ovle.rll3.model.ecs.entity.positions
 import com.ovle.rll3.model.ecs.entity.newTemplatedEntity
 import com.ovle.rll3.model.ecs.entity.randomId
-import com.ovle.rll3.model.template.SpawnTemplate
+import com.ovle.rll3.model.procedural.grid.utils.SpawnTable
 import com.ovle.rll3.model.template.entity.EntityTemplates
 import com.ovle.rll3.model.tile.*
-import com.ovle.rll3.model.util.rotate180
-import com.ovle.rll3.model.util.rotate270
-import com.ovle.rll3.model.util.rotate90
 import com.ovle.rll3.point
 import ktx.ashley.get
 
@@ -34,63 +30,31 @@ class EntityProcessor(val templates: EntityTemplates) : TilesProcessor {
                 if (point(x, y) in claimed) continue
 
                 val nearTiles = nearValues(tiles, x, y)
-                val spawnDatas = spawnTemplates
-                    .map { it to it.spawns.filter { spawn -> matchesTemplate(spawn, nearTiles) } }
-                    .filter { (_, spawns) -> spawns.isNotEmpty() }
-
-                //todo spawn table
-                val spawnData = spawnDatas.randomOrNull(r) ?: continue
-                val spawnTemplate = spawnData.first
-                val spawnCondition = spawnData.second.random(r)
+                val spawnTable = SpawnTable(spawnTemplates, nearTiles, worldInfo.r)
                 val check = r.nextDouble()
+                val spawnTemplate = spawnTable.spawn(check) ?: continue
 
-                val haveSameEntityNear = haveSameEntityNear(spawnTemplate.name, x, y, spawnCondition.groupRadius, entities)
-                var chance = spawnCondition.chance
-                if (haveSameEntityNear) chance += spawnCondition.groupBonusChance
-                val needSpawn = check <= chance
+//                val spawnCondition = spawnData.second
+//                val haveSameEntityNear = haveSameEntityNear(spawnTemplate.name, x, y, spawnCondition.groupRadius, entities)
+//                var chance = spawnCondition.chance
+//                if (haveSameEntityNear) chance += spawnCondition.groupBonusChance
+//                val needSpawn = check <= chance
 
-                if (needSpawn) {
-                    val spawnPosition = point(x, y)
-                    val id = randomId()
-                    val entity = newTemplatedEntity(id, spawnTemplate, gameEngine)
-                    entity[position]?.gridPosition = spawnPosition
-                    entities.add(entity)
-                }
+                val spawnPosition = point(x, y)
+                val id = randomId()
+                val entity = newTemplatedEntity(id, spawnTemplate, gameEngine)
+                entity[position]?.gridPosition = spawnPosition
+                entities.add(entity)
             }
         }
 
         levelInfo.entities.plusAssign(entities)
     }
 
-    private fun haveSameEntityNear(templateName: String, x: Int, y: Int, radius: Int, entities: MutableList<Entity>): Boolean {
-        val sameEntities = entities.filter { it[template]?.template?.name == templateName }
-        val nearRangeX = (x - radius)..(x + radius)
-        val nearRangeY = (y - radius)..(y + radius)
-        return sameEntities.map { it[position]!!.gridPosition }.any { it.x in nearRangeX && it.y in nearRangeY }
-    }
-
-    private fun matchesTemplate(spawnTemplate: SpawnTemplate, nearTiles: NearValues<Tile?>): Boolean {
-        val parsedMask = spawnTemplate.parsedMask ?: return true
-        if (!spawnTemplate.rotate) {
-            val maskTiles = parsedMask.reversed().flatten()
-            return matchesMask(maskTiles, nearTiles)
-        }
-
-        val maskTilesR = rotate180(parsedMask).flatten()
-        val maskTilesR1 = rotate90(parsedMask).flatten()
-        val maskTilesR2 = parsedMask.flatten()
-        val maskTilesR3 = rotate270(parsedMask).flatten()
-
-        return matchesMask(maskTilesR, nearTiles) ||
-            matchesMask(maskTilesR1, nearTiles) ||
-            matchesMask(maskTilesR2, nearTiles) ||
-            matchesMask(maskTilesR3, nearTiles)
-    }
-
-    private fun matchesMask(maskTiles: List<TileType>, nearTiles: NearValues<Tile?>): Boolean {
-        val actualTiles = nearTiles.asList.flatten().map { it?.typeId }
-        return maskTiles
-            .zip(actualTiles)
-            .all { (maskTile, actualTile) -> maskTile == whateverTileId || maskTile == actualTile }
-    }
+//    private fun haveSameEntityNear(templateName: String, x: Int, y: Int, radius: Int, entities: MutableList<Entity>): Boolean {
+//        val sameEntities = entities.filter { it[template]?.template?.name == templateName }
+//        val nearRangeX = (x - radius)..(x + radius)
+//        val nearRangeY = (y - radius)..(y + radius)
+//        return sameEntities.map { it[position]!!.gridPosition }.any { it.x in nearRangeX && it.y in nearRangeY }
+//    }
 }
---
+++ core/src/main/com/ovle/rll3/model/procedural/grid/utils/SpawnTable.kt
@@ -0,0 +1,66 @@
+package com.ovle.rll3.model.procedural.grid.utils
+
+import com.ovle.rll3.model.template.SpawnTemplate
+import com.ovle.rll3.model.template.entity.EntityTemplate
+import com.ovle.rll3.model.tile.*
+import com.ovle.rll3.model.util.rotate180
+import com.ovle.rll3.model.util.rotate270
+import com.ovle.rll3.model.util.rotate90
+import kotlin.random.Random
+
+class SpawnTable(spawnTemplates: List<EntityTemplate>, nearTiles: NearTiles, val r: Random) {
+
+    private val spawnData = spawnTemplates
+        .map { it to it.spawns.filter { spawn -> matchesTemplate(spawn, nearTiles) } }
+        .filter { (_, spawns) -> spawns.isNotEmpty() }
+
+
+    @OptIn(ExperimentalStdlibApi::class)
+    fun spawn(check: Double): EntityTemplate? {
+        if (spawnData.isEmpty()) return null
+
+        val spawns = spawnData.flatMap { it.second }
+        val chanceTotal = spawns.size
+//        val chanceTotalPositive = spawns.sumByDouble { it.chance.toDouble() }
+        val coeff = 1.0 / chanceTotal.toDouble()    //todo high spawn chance for particular template
+                                                    // can be very decreased by number of templates this way
+        val table = spawns.map { it.chance * coeff }
+
+        var value = check
+        var spawnIndex = 0
+        for (chance in table) {
+            value -= chance
+            if (value < 0) break
+
+            spawnIndex++
+        }
+        if (spawnIndex >= spawnData.size) return null
+
+        return spawnData[spawnIndex].first
+    }
+
+    private fun matchesTemplate(spawnTemplate: SpawnTemplate, nearTiles: NearValues<Tile?>): Boolean {
+        val parsedMask = spawnTemplate.parsedMask ?: return true
+        if (!spawnTemplate.rotate) {
+            val maskTiles = parsedMask.reversed().flatten()
+            return matchesMask(maskTiles, nearTiles)
+        }
+
+        val maskTilesR = rotate180(parsedMask).flatten()
+        val maskTilesR1 = rotate90(parsedMask).flatten()
+        val maskTilesR2 = parsedMask.flatten()
+        val maskTilesR3 = rotate270(parsedMask).flatten()
+
+        return matchesMask(maskTilesR, nearTiles) ||
+            matchesMask(maskTilesR1, nearTiles) ||
+            matchesMask(maskTilesR2, nearTiles) ||
+            matchesMask(maskTilesR3, nearTiles)
+    }
+
+    private fun matchesMask(maskTiles: List<TileType>, nearTiles: NearValues<Tile?>): Boolean {
+        val actualTiles = nearTiles.asList.flatten().map { it?.typeId }
+        return maskTiles
+            .zip(actualTiles)
+            .all { (maskTile, actualTile) -> maskTile == whateverTileId || maskTile == actualTile }
+    }
+}
