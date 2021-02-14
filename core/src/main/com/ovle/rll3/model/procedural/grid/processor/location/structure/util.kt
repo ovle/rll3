7723c6ab4a1854d444fc87b116da0bdba0acf5a5
ovle <owlywhy@gmail.com>
2021-02-15 01:13:06 +0700

    fixed structure spawn bug

--- core/src/main/com/ovle/rll3/model/procedural/grid/processor/location/structure/util.kt
+++ core/src/main/com/ovle/rll3/model/procedural/grid/processor/location/structure/util.kt
@@ -5,6 +5,7 @@ import com.ovle.rlUtil.TileArray
 import com.ovle.rlUtil.TileCondition
 import com.ovle.rlUtil.cartesianProduct
 import com.ovle.rlUtil.gdx.math.point
+import com.ovle.rll3.model.util.info
 import kotlin.random.Random
 
 fun TileArray.setArray(index: GridPoint2, value: TileArray, tilePostFilter: TileCondition?) {
@@ -19,6 +20,8 @@ fun TileArray.setArray(index: GridPoint2, value: TileArray, tilePostFilter: Tile
     }
 }
 
+//todo test
+//todo move to rl-util
 fun spawnPoint(random: Random, tiles: TileArray, structureSize: Int, tileFilter: TileCondition?, maxAttempts: Int): GridPoint2? {
     fun isTileSpawnAllowed(tiles: TileArray, x: Int, y: Int) = tileFilter?.invoke(tiles[x, y]) ?: true
 
@@ -29,9 +32,13 @@ fun spawnPoint(random: Random, tiles: TileArray, structureSize: Int, tileFilter:
         val x = (0..widthDiff).random(random)
         val y = (tiles.size - 1) - (0..heightDiff).random(random)
 
-        val checkpointsX = arrayOf(x, x + structureSize/2, x + structureSize)
-        val checkpointsY = arrayOf(y, y - structureSize/2, y - structureSize)
+        val checkpointsX = arrayOf(x, x + (structureSize - 1)/2, x + (structureSize - 1))
+        val checkpointsY = arrayOf(y, y - (structureSize - 1)/2, y - (structureSize - 1))
+//        println("x: $x, y: $y")
+//        println("checkpointsX: ${checkpointsX.joinToString { it.toString() }}")
+//        println("checkpointsY: ${checkpointsY.joinToString { it.toString() }}")
         val checkpoints = cartesianProduct(checkpointsX, checkpointsY)
+
         val isSpawnAllowed = checkpoints.all { (cx, cy) -> isTileSpawnAllowed(tiles, cx, cy) }
         if (isSpawnAllowed) {
             result = point(x, y)
